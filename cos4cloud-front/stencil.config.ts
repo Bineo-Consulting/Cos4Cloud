729d0b6f4dddcb4456036b9e789fcfe71e4d8621
Robin Giles Ribera <robinchogiles@gmail.com>
2021-03-24 12:05:34 +0100

    WIP

--- cos4cloud-front/stencil.config.ts
+++ cos4cloud-front/stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 import replace from '@rollup/plugin-replace';
 
-const cloudHost = 'https://europe-west2-cos4cloud-2d9d3.cloudfunctions.net'
+const cloudHost = 'https://europe-west2-cos4cloud-2d9d3.cloudfunctions.net/api'
 const dev: boolean = process.argv && process.argv.indexOf('--dev') > -1;
 const apiEnv: string = dev ? 'dev' : 'prod';
 const HOST = process.env.HOST || (dev ? 'http://localhost:5001/cos4cloud-2d9d3/europe-west2/api' : cloudHost)
