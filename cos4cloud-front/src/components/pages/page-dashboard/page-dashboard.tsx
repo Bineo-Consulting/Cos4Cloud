d4acbfbf29ba4144649323d2a67125ca37813172
Robin Giles Ribera <robinchogiles@gmail.com>
2021-10-25 16:41:57 +0200

    WIP

--- cos4cloud-front/src/components/pages/page-dashboard/page-dashboard.tsx
+++ cos4cloud-front/src/components/pages/page-dashboard/page-dashboard.tsx
@@ -2,6 +2,18 @@ import { Component, Host, Prop, State, h } from '@stencil/core';
 import { fetchTranslations } from '../../../utils/translation'
 import resources from '../../../resources'
 
+const counter = (val) => {
+  if (val < 10) {
+    return {agg: [val, 10 - val], total: 12}
+  } else if (val < 100) {
+    return {agg: [val, 100 - val], total: 120}
+  } else if (val < 1000) {
+    return {agg: [val, 1000 - val], total: 1200}
+  } else if (val < 10000) {
+    return {agg: [val, 10000 - val], total: 12000}
+  }
+}
+
 @Component({
   tag: 'page-dashboard',
   styleUrl: 'page-dashboard.css',
@@ -27,6 +39,7 @@ export class PageDashboard {
   }
 
   agg() {
+
     fetch(resources.host + '/comments/agg')
     .then(res => res.json())
     .then(res => {
@@ -37,6 +50,11 @@ export class PageDashboard {
           el: this.charts.origins,
           agg: res.origins
         })
+        this.setChartCounter({
+          el: this.charts.commentsCountEl,
+          count: (res.comments_count || 0) + (res.identifications_count || 0),
+          title: `Comments &<br> Identifications`
+        })
       }, 250)
     })
 
@@ -45,6 +63,10 @@ export class PageDashboard {
     .then(res => {
       this.downloadsAgg = res
       setTimeout(() => {
+        this.setChartCounter({
+          el: this.charts.identificationsCountEl,
+          count: res.downloads_count,
+          title: 'Downloads'})
         this.setPeriodDownloads(this.periodDownloads)
         this.setPie({
           el: this.charts.reasons,
@@ -58,12 +80,12 @@ export class PageDashboard {
     .then(res => {
       this.usersAgg = res
       setTimeout(() => {
-        console.log({professions: res.professions})
         this.setPeriodUsers(this.periodUsers)
         this.setPie({
           el: this.charts.professions,
           agg: res.professions
         })
+        this.setChartCounter({el: this.charts.usersCountEl, count: res.users_count || 0, title: 'Users'})
       }, 250)
     })
   }
@@ -137,16 +159,14 @@ export class PageDashboard {
   }
 
   async setPie({el, agg}) {
-    console.log('setPie => ', {el, agg})
     const Chartist = await import('chartist')
     const ChartistPluginLegend = (await import('chartist-plugin-legend')).default
-    //const chartistPluginTooltip = (await import ('chartist-plugin-tooltip')).default
-
+    const total = agg.map(i => i.count).reduce((a, b) => b + a, 0)
     new Chartist.Pie(el, {
-      labels: agg.map(_ => ' '),
+      labels: agg.map(_ => _.count),
       series: agg.map(i => i.count)
     }, {
-      donut: true,
+      donut: false,
       donutWidth: 60,
       donutSolid: true,
       startAngle: 270,
@@ -155,22 +175,22 @@ export class PageDashboard {
         ChartistPluginLegend({
           legendNames: agg.map(i => (i._id || 'Other'))
         })
-      ]
+      ],
+      labelInterpolationFnc: function(value) {
+        return Math.round(value / total * 100) + '%';
+      }
     });
   }
 
   async setBar({el, agg, labels}) {
     const Chartist = await import('chartist')
     const ChartistPluginLegend = (await import('chartist-plugin-legend')).default
-    // await import('chartist-plugin-legend')
 
     new Chartist.Bar(el, {
       labels,
       series: agg
     }, {
-      // high: 14,
       seriesBarDistance: 10,
-      // distributeSeries: true,
       axisY: {
         onlyInteger: true,
         offset: 20
@@ -197,11 +217,84 @@ export class PageDashboard {
       }
     });
   }
+
+  async setChartCounter({ el, count, title }) {
+    const Chartist = await import('chartist')
+    const ChartistPluginFillDonut = (await import('chartist-plugin-fill-donut')).default
+
+    const { agg, total } = counter(count)
+
+    console.log('counter', {agg, total})
+    const chart = new Chartist.Pie(el, {
+        series: agg,
+        labels: ['', '']
+      }, {
+        donut: true,
+        donutWidth: 20,
+        startAngle: 210,
+        total: total,
+        showLabel: false,
+        plugins: [
+          ChartistPluginFillDonut({
+            items: [{
+              content: '<ion-icon name="speedometer"></ion-icon>',
+              position: 'bottom',
+              offsetY : 10,
+              offsetX: -8,
+              fontSize: '20px'
+            }, {
+              position: 'center',
+              offsetY : -10,
+              content: `<h3><span class="small">${count} <br>${title}</span></h3>`
+            }]
+          })
+        ]
+      });
+
+    chart.on('draw', function(data) {
+      if (data.type === 'slice' && data.index == 0) {
+        // Get the total path length in order to use for dash array animation
+        const pathLength = data.element._node.getTotalLength();
+
+        // Set a dasharray that matches the path length as prerequisite to animate dashoffset
+        data.element.attr({
+          'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
+        });
+
+        // Create animation definition while also assigning an ID to the animation for later sync usage
+        const animationDefinition = {
+          'stroke-dashoffset': {
+            id: 'anim' + data.index,
+            dur: 1200,
+            from: -pathLength + 'px',
+            to:  '0px',
+            easing: Chartist.Svg.Easing.easeOutQuint,
+            fill: 'freeze'
+          }
+        };
+
+        // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
+        data.element.attr({
+          'stroke-dashoffset': -pathLength + 'px'
+        });
+
+        // We can't use guided mode as the animations need to rely on setting begin manually
+        // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
+        data.element.animate(animationDefinition, true);
+      }
+    });
+  }
+
   render() {
     return (
       <Host>
         <div class="dashboard-container">
           <ion-title><h2>Dashboard</h2></ion-title>
+
+          <span ref={(el) => this.charts.commentsCountEl = (el as HTMLElement)} class="ct-chart ct-chart-counter"></span>
+          <span ref={(el) => this.charts.identificationsCountEl = (el as HTMLElement)} class="ct-chart ct-chart-counter"></span>
+          <span ref={(el) => this.charts.usersCountEl = (el as HTMLElement)} class="ct-chart ct-chart-counter"></span>
+
 
           {this.commentsAgg && <div class="charts">
 
@@ -215,12 +308,12 @@ export class PageDashboard {
                 <li onClick={() => this.setPeriodComments('pAll')} class={'pAll' === this.periodComments ? 'active' : ''}>ALL</li>
               </ul>
             </div>
-            <span ref={(el) => this.charts.comments12M = (el as HTMLElement)} class="ct-chart chart-bar mb-100"></span>
+            <span ref={(el) => this.charts.comments12M = (el as HTMLElement)} class="ct-chart ct-style-one chart-bar mb-100"></span>
 
             <div class="cnt-header-chart">
               <ion-title class="title-chart"><b>{this.i18n.stats.comm_id_by_portal}</b></ion-title>
             </div>
-            <span ref={(el) => this.charts.origins = (el as HTMLElement)} class="ct-chart"></span>
+            <span ref={(el) => this.charts.origins = (el as HTMLElement)} class="ct-chart ct-style-one"></span>
           </div>}
 
           {this.downloadsAgg && <div class="charts">
@@ -235,13 +328,13 @@ export class PageDashboard {
                 <li onClick={() => this.setPeriodDownloads('pAll')} class={'pAll' === this.periodDownloads ? 'active' : ''}>ALL</li>
               </ul>
             </div>
-            <span ref={(el) => this.charts.downloads12M = (el as HTMLElement)} class="ct-chart chart-bar mb-100"></span>
+            <span ref={(el) => this.charts.downloads12M = (el as HTMLElement)} class="ct-chart ct-style-one chart-bar mb-100"></span>
             
 
             <div class="cnt-header-chart">
               <ion-title class="title-chart"><b>{this.i18n.stats.downloads_reasons}</b></ion-title>
             </div>
-            <span ref={(el) => this.charts.reasons = (el as HTMLElement)} class="ct-chart"></span>
+            <span ref={(el) => this.charts.reasons = (el as HTMLElement)} class="ct-chart ct-style-one"></span>
 
           </div>}
 
@@ -257,13 +350,13 @@ export class PageDashboard {
                 <li onClick={() => this.setPeriodUsers('pAll')} class={'pAll' === this.periodUsers ? 'active' : ''}>ALL</li>
               </ul>
             </div>
-            <span ref={(el) => this.charts.users12M = (el as HTMLElement)} class="ct-chart chart-bar mb-100"></span>
+            <span ref={(el) => this.charts.users12M = (el as HTMLElement)} class="ct-chart ct-style-one chart-bar mb-100"></span>
             
 
             <div class="cnt-header-chart">
               <ion-title class="title-chart"><b>Professions</b></ion-title>
             </div>
-            <span ref={(el) => this.charts.professions = (el as HTMLElement)} class="ct-chart last-chart"></span>
+            <span ref={(el) => this.charts.professions = (el as HTMLElement)} class="ct-chart ct-style-one last-chart"></span>
 
           </div>}
         </div>
